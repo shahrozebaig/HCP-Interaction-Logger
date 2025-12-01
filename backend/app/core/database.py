5cb31afb01b78ff8fdcdd1c012b28c0a24b1fe35
Shahroze Baig <150318142+shahrozebaig@users.noreply.github.com>
2025-12-02 01:30:24 +0530

    Update database.py

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,26 +1,21 @@
-# backend/app/core/database.py
-
 from motor.motor_asyncio import AsyncIOMotorClient
 from .config import settings
 
 client: AsyncIOMotorClient | None = None
 db = None
 
-
 async def connect_to_mongo():
     global client, db
     client = AsyncIOMotorClient(str(settings.MONGODB_URI))
     db = client[settings.MONGODB_DB]
 
-    # Optional: Ensure indexes
     await db.hcp.create_index("name")
     await db.interactions.create_index("hcpId")
-
     print("Connected to MongoDB")
-
 
 async def close_mongo_connection():
     global client
     if client:
         client.close()
         print("MongoDB connection closed")
+        
