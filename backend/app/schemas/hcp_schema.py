03cc7b69073b895dc74dbeceb1a987cf1bc24bba
Shahroze Baig <150318142+shahrozebaig@users.noreply.github.com>
2025-12-02 01:36:29 +0530

    Update hcp_schema.py

--- backend/app/schemas/hcp_schema.py
+++ backend/app/schemas/hcp_schema.py
@@ -1,17 +1,13 @@
-# backend/app/schemas/hcp_schema.py
 from pydantic import BaseModel, Field
 from typing import Optional
-
 
 class HCPBase(BaseModel):
     name: str = Field(...)
     speciality: Optional[str] = None
     location: Optional[str] = None
 
-
 class HCPCreate(HCPBase):
     pass
-
 
 class HCPInDB(HCPBase):
     id: str
